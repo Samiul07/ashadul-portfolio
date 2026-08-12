3ce07c149eaacb8c78c6b83f9e9fd5a3f5278c40
Ashadul Islam Samiul <ashadulislamamiul@gmail.com>
2026-08-13 02:25:05 +0600

    feat: inject high-res source PNG for hero image

--- components/sections/why-me-portrait-frame.tsx
+++ components/sections/why-me-portrait-frame.tsx
@@ -49,7 +49,7 @@ export default function WhyMePortraitFrame({
           height={1537}
           loading="lazy"
           sizes={sizes}
-          src="/images/why-me-blue-portrait.png"
+          src="/images/why-me-portrait.png"
           width={1023}
           quality={90}
         />
@@ -88,7 +88,7 @@ export default function WhyMePortraitFrame({
             height={1537}
             loading="lazy"
             sizes={sizes}
-            src="/images/why-me-blue-portrait.png"
+            src="/images/why-me-portrait.png"
             width={1023}
             quality={90}
           />
